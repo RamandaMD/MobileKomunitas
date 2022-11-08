86e7034f7e462941d38345302e509ff59d0ff142
RamandaMD <ramandamd@mhs.mdp.ac.id>
2022-11-08 16:45:12 +0700

    Mobile Komunitas

---
+++ app/src/main/java/com/if3a/mobilekomunitas/ActivitySplashScreen.java
@@ -0,0 +1,28 @@
+package com.if3a.mobilekomunitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.window.SplashScreen;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+        , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {  @Override
+        public void run() {
+            startActivity(new Intent(ActivitySplashScreen.this, MainActivity.class));
+            finish();
+            }
+        }, 2000);
+    }
+}
